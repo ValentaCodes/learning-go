98d3c2272371b5f51f875c35bda5b196a4167c2e
ValentaCodes <valentacodes@gmail.com>
2023-09-24 21:21:03 -0600

    first test file

---
+++ first.go
@@ -0,0 +1,9 @@
+// GO is multi-platform
+// IT can generate executable binary files for different platforms and os's
+// IT can compile to WebAssembly (WASM)
+// IT can transpile to front-end JS (GopherJS)
+package main
+
+func main() {
+	print("Hello from Go")
+}
