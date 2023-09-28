cfe050bc2049a0324b5c225a4c8b778c003553e0
ValentaCodes <valentacodes@gmail.com>
2023-09-28 15:15:49 -0600

    new function call and import

--- hello/hello.go
+++ hello/hello.go
@@ -4,11 +4,16 @@ import (
 	"fmt"
 
 	"example.com/greetings"
+	"golang.org/x/example/hello/reverse"
 )
 
 func main() {
 	// Get a greeting message and print it
 	message := greetings.Hello("Bobby")
 	fmt.Println(message)
+	reverseStringAndInt()
+}
 
+func reverseStringAndInt() {
+	fmt.Println(reverse.String("Hello World"), reverse.Int(2345), reverse.Float(23234.32))
 }
